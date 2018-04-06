fa6e44d2d3b78727e2f1d1eefaedbaac4df02611
SamuelOjes <kingojes@gmail.com>
2018-04-06 19:19:42 +0100

    improved on the layout and fixed some errors in my code

--- app/src/main/java/com/example/kingoj/login_screen/ForGotPasswordActivity.java
+++ app/src/main/java/com/example/kingoj/login_screen/ForGotPasswordActivity.java
@@ -1,13 +1,37 @@
 package com.example.kingoj.login_screen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ForGotPasswordActivity extends AppCompatActivity {
+
+   // private Button mtGoBack; Would attend to this Later
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_for_got_password);
+    }
+
+   /* public void setMtGoBack(Button mtGoBack) {
+        this.mtGoBack = mtGoBack;
+
+        mtGoBack = (Button) findViewById(R.id.backsignin);
+        mtGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+    */
+
+    public void mainActivity(View view){
+        //Perform Return To Main Activity When Clicked
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
